1d344ce3c96bcc96c59bbc2524db89857366af83
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-01-19 16:53:32 +0330

    Add bottom navigation support

--- mobile/src/main/java/com/phelat/tedu/MainActivity.kt
+++ mobile/src/main/java/com/phelat/tedu/MainActivity.kt
@@ -2,12 +2,32 @@ package com.phelat.tedu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.phelat.tedu.uiview.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setupBottomNavigationView()
+        }
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        bottomNavigationView.setupWithNavController(
+            listOf(R.navigation.navigation_todolist),
+            supportFragmentManager,
+            R.id.fragmentContainer,
+            intent
+        ).observe(this, Observer {})
+    }
 }
