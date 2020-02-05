dd0cecc9785909e88b3a8149aa024260c104974a
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-02-05 16:38:05 +0330

    Use flowOn operator to use IO dispatcher

--- feature/todolist/src/main/java/com/phelat/tedu/todolist/view/TodoListFragment.kt
+++ feature/todolist/src/main/java/com/phelat/tedu/todolist/view/TodoListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.phelat.tedu.daggerandroid.Injector
@@ -16,9 +17,9 @@ import com.phelat.tedu.todolist.di.component.TodoListComponent
 import kotlinx.android.synthetic.main.fragment_todolist.addTodoButton
 import kotlinx.android.synthetic.main.fragment_todolist.todoListRecycler
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -45,15 +46,14 @@ class TodoListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         with(todoListRecycler) {
             layoutManager = LinearLayoutManager(requireContext())
-            GlobalScope.launch {
-                withContext(Dispatchers.IO) {
-                    todoReadableDataSource.read()
-                        .collect {
-                            withContext(Dispatchers.Main) {
-                                adapter = TodoListAdapter(it)
-                            }
+            lifecycleScope.launch {
+                todoReadableDataSource.read()
+                    .flowOn(Dispatchers.IO)
+                    .collect {
+                        withContext(Dispatchers.Main) {
+                            adapter = TodoListAdapter(it)
                         }
-                }
+                    }
             }
         }
         addTodoButton.setOnClickListener {
