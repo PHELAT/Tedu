f3e46e8b034548d43699df783df7611474b8c29d
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-02-05 15:35:52 +0330

    Rename AddTodoDispatcherScope to AddTodoScope

--- feature/addtodo/src/main/java/com/phelat/tedu/addtodo/di/component/AddTodoComponent.kt
+++ feature/addtodo/src/main/java/com/phelat/tedu/addtodo/di/component/AddTodoComponent.kt
@@ -1,13 +1,13 @@
 package com.phelat.tedu.addtodo.di.component
 
 import com.phelat.tedu.addtodo.di.module.AddTodoModule
-import com.phelat.tedu.addtodo.di.scope.AddTodoDispatcherScope
+import com.phelat.tedu.addtodo.di.scope.AddTodoScope
 import com.phelat.tedu.daggerandroid.DispatcherComponent
 import com.phelat.tedu.todo.di.component.TodoComponent
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 
-@AddTodoDispatcherScope
+@AddTodoScope
 @Component(
     modules = [AndroidInjectionModule::class, AddTodoModule::class],
     dependencies = [TodoComponent::class]
--- feature/addtodo/src/main/java/com/phelat/tedu/addtodo/di/scope/AddTodoScope.kt
+++ feature/addtodo/src/main/java/com/phelat/tedu/addtodo/di/scope/AddTodoScope.kt
@@ -4,4 +4,4 @@ import javax.inject.Scope
 
 @Scope
 @Retention(AnnotationRetention.RUNTIME)
-annotation class AddTodoDispatcherScope
+annotation class AddTodoScope
