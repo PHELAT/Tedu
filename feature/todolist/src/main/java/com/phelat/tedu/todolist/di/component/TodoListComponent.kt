670de58b9bc1d63e1c833ff0dbb88fdbb5e75052
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-02-05 15:38:18 +0330

    Rename TodoListDispatcherScope to TodoListScope

--- feature/todolist/src/main/java/com/phelat/tedu/todolist/di/component/TodoListComponent.kt
+++ feature/todolist/src/main/java/com/phelat/tedu/todolist/di/component/TodoListComponent.kt
@@ -3,11 +3,11 @@ package com.phelat.tedu.todolist.di.component
 import com.phelat.tedu.daggerandroid.DispatcherComponent
 import com.phelat.tedu.todo.di.component.TodoComponent
 import com.phelat.tedu.todolist.di.module.TodoListModule
-import com.phelat.tedu.todolist.di.scope.TodoListDispatcherScope
+import com.phelat.tedu.todolist.di.scope.TodoListScope
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 
-@TodoListDispatcherScope
+@TodoListScope
 @Component(
     modules = [AndroidInjectionModule::class, TodoListModule::class],
     dependencies = [TodoComponent::class]
