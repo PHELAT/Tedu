a25eaa8b1e0817328cd7625ba7daac580a217253
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-03-13 16:06:01 +0330

    Add threetenabp library

--- platform/mobile/src/main/java/com/phelat/tedu/Tedu.kt
+++ platform/mobile/src/main/java/com/phelat/tedu/Tedu.kt
@@ -1,6 +1,7 @@
 package com.phelat.tedu
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.phelat.tedu.addtodo.di.module.AddTodoModule
 import com.phelat.tedu.dependencyinjection.installFeatures
 import com.phelat.tedu.todolist.di.module.TodoListModule
@@ -11,6 +12,7 @@ class Tedu : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AndroidThreeTen.init(this)
         startKoin {
             androidContext(this@Tedu)
             installFeatures(TodoListModule, AddTodoModule)
