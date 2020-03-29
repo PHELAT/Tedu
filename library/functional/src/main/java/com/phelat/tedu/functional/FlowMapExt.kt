8f2b3141d8537b5e9769f5f46d5170ab55364979
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-03-29 23:59:03 +0430

    Cleanup `mapForEach` function

--- library/functional/src/main/java/com/phelat/tedu/functional/FlowMapExt.kt
+++ library/functional/src/main/java/com/phelat/tedu/functional/FlowMapExt.kt
@@ -5,7 +5,9 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
 
 @OptIn(ExperimentalCoroutinesApi::class)
-inline fun <T, R> Flow<List<T>>.mapForEach(crossinline transform: (value: T) -> R): Flow<List<R>> = transform { value ->
-    val mappedList = value.map { transform(it) }
-    return@transform emit(mappedList)
+inline fun <T, R> Flow<List<T>>.mapForEach(crossinline transform: (value: T) -> R): Flow<List<R>> {
+    return transform { value ->
+        val mappedList = value.map { transform(it) }
+        return@transform emit(mappedList)
+    }
 }
