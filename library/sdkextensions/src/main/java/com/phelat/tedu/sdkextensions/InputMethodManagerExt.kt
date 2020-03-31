0eccdaa97052929cdc035536a31af672f9179e43
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-03-31 20:12:17 +0430

    Delay 64ms before requesting edit text to focus in `showKeyboard`

--- library/sdkextensions/src/main/java/com/phelat/tedu/sdkextensions/InputMethodManagerExt.kt
+++ library/sdkextensions/src/main/java/com/phelat/tedu/sdkextensions/InputMethodManagerExt.kt
@@ -4,9 +4,16 @@ import android.content.Context
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+
+private const val INPUT_FOCUS_DELAY_IN_MILLIS = 64L
 
 fun Fragment.showKeyboard(inputToFocus: EditText) {
     (requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
         .toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
-    inputToFocus.requestFocus()
+    lifecycleScope.launchWhenResumed {
+        delay(INPUT_FOCUS_DELAY_IN_MILLIS)
+        inputToFocus.requestFocus()
+    }
 }
