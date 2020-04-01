2b28db1c99f4d83bffde01fdcee8b11800e76669
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-04-01 22:31:13 +0430

    Change T type from List to Iterable in `mapForEach` function

--- library/functional/src/main/java/com/phelat/tedu/functional/FlowMapExt.kt
+++ library/functional/src/main/java/com/phelat/tedu/functional/FlowMapExt.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
 
 @OptIn(ExperimentalCoroutinesApi::class)
-inline fun <T, R> Flow<List<T>>.mapForEach(crossinline transform: (value: T) -> R): Flow<List<R>> {
+inline fun <T, R> Flow<Iterable<T>>.mapForEach(crossinline transform: (value: T) -> R): Flow<List<R>> {
     return transform { value ->
         val mappedList = value.map { transform(it) }
         return@transform emit(mappedList)
