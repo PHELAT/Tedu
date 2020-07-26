fde2abfe057f877f318d31694516696618bcb3dc
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-07-26 23:34:11 +0430

    Implement ContributorsFragment.kt

---
+++ feature/contributors/src/main/java/com/phelat/tedu/contributors/view/ContributorsFragment.kt
@@ -0,0 +1,6 @@
+package com.phelat.tedu.contributors.view
+
+import androidx.fragment.app.Fragment
+import com.phelat.tedu.contributors.R
+
+class ContributorsFragment : Fragment(R.layout.fragment_contributors)
