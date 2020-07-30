0a581d0f0ff6115c4c725f55045dfdc6f964f9f8
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-07-30 22:21:37 +0430

    Set logging interceptor for OkHttpClient in debug builds

--- library/networking/src/main/java/com/phelat/tedu/networking/di/module/NetworkModule.kt
+++ library/networking/src/main/java/com/phelat/tedu/networking/di/module/NetworkModule.kt
@@ -1,9 +1,11 @@
 package com.phelat.tedu.networking.di.module
 
 import com.phelat.tedu.dependencyinjection.library.LibraryScope
+import com.phelat.tedu.networking.BuildConfig
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 @Module
@@ -11,12 +13,25 @@ internal class NetworkModule {
 
     @Provides
     @LibraryScope
-    fun provideOkHttpClient(): OkHttpClient {
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides
+    @LibraryScope
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
             .callTimeout(CALL_TIME_OUT_IN_MILLIS, TimeUnit.MILLISECONDS)
             .connectTimeout(CONNECT_TIME_OUT_IN_MILLIS, TimeUnit.MILLISECONDS)
             .readTimeout(READ_TIME_OUT_IN_MILLIS, TimeUnit.MILLISECONDS)
             .writeTimeout(WRITE_TIME_OUT_IN_MILLIS, TimeUnit.MILLISECONDS)
+            .addInterceptor(loggingInterceptor)
             .build()
     }
 
