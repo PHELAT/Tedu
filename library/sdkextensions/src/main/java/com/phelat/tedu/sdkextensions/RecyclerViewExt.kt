a46f35fd30f49ea84605571f0c11e549f4405cb1
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-07-31 21:00:59 +0430

    Implement `onReachTheEnd` extension function on RecyclerView

---
+++ library/sdkextensions/src/main/java/com/phelat/tedu/sdkextensions/RecyclerViewExt.kt
@@ -0,0 +1,18 @@
+package com.phelat.tedu.sdkextensions
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+inline fun RecyclerView.onReachTheEnd(crossinline block: () -> Unit) {
+    val scrollListener: RecyclerView.OnScrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            val visibleItemCount: Int = requireNotNull(layoutManager).childCount
+            val totalItemCount: Int = requireNotNull(layoutManager).itemCount
+            val pastVisibleItems: Int = (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+            if (pastVisibleItems + visibleItemCount + 2 >= totalItemCount) {
+                block.invoke()
+            }
+        }
+    }
+    addOnScrollListener(scrollListener)
+}
