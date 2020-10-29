a3653bd56e51b5948c2e44b658e6e226445c152f
Your Name <you@example.com>
2020-10-29 18:13:00 +0330

    comment crashlytics

--- common/analytics/src/main/java/com/phelat/tedu/analytics/logger/NonFatalExceptionLogger.kt
+++ common/analytics/src/main/java/com/phelat/tedu/analytics/logger/NonFatalExceptionLogger.kt
@@ -4,15 +4,16 @@ import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.phelat.tedu.analytics.BuildConfig
 import com.phelat.tedu.analytics.ExceptionLogger
 import com.phelat.tedu.dependencyinjection.common.CommonScope
+import dagger.Lazy
 import javax.inject.Inject
 
 @CommonScope
 class NonFatalExceptionLogger @Inject constructor(
-    private val crashlytics: FirebaseCrashlytics
+    private val crashlytics: Lazy<FirebaseCrashlytics>
 ) : ExceptionLogger {
 
     override fun log(throwable: Throwable) {
-        crashlytics.recordException(throwable)
+//        crashlytics.recordException(throwable)
         if (BuildConfig.DEBUG) {
             throwable.printStackTrace()
         }
