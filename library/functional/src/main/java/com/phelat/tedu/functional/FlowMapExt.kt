5d879422a6ea0aa094e77269bd9477d1518de475
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-12-18 21:55:43 +0330

    Update coroutines from 1.3.5 to 1.4.2

--- library/functional/src/main/java/com/phelat/tedu/functional/FlowMapExt.kt
+++ library/functional/src/main/java/com/phelat/tedu/functional/FlowMapExt.kt
@@ -1,10 +1,8 @@
 package com.phelat.tedu.functional
 
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
 
-@OptIn(ExperimentalCoroutinesApi::class)
 inline fun <T, R> Flow<Iterable<T>>.mapForEach(
     crossinline transform: (value: T) -> R
 ): Flow<List<R>> {
