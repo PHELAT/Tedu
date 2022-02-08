04a6ce45462b7212e23b3c9037d1b5d3d2d6ea37
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2022-02-08 22:21:04 +0100

    Make the project buildable once again

--- feature/contributors/src/main/java/com/phelat/tedu/contributors/view/PaginationLoadingItem.kt
+++ feature/contributors/src/main/java/com/phelat/tedu/contributors/view/PaginationLoadingItem.kt
@@ -1,13 +1,18 @@
 package com.phelat.tedu.contributors.view
 
+import android.view.View
 import com.phelat.tedu.contributors.R
-import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
-import com.xwray.groupie.kotlinandroidextensions.Item
+import com.phelat.tedu.contributors.databinding.ItemPaginationLoadingBinding
+import com.xwray.groupie.viewbinding.BindableItem
 
-class PaginationLoadingItem : Item() {
+class PaginationLoadingItem : BindableItem<ItemPaginationLoadingBinding>() {
 
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+    override fun bind(viewHolder: ItemPaginationLoadingBinding, position: Int) {
         // No-op!
+    }
+
+    override fun initializeViewBinding(view: View): ItemPaginationLoadingBinding {
+        return ItemPaginationLoadingBinding.bind(view)
     }
 
     override fun getLayout(): Int = R.layout.item_pagination_loading
