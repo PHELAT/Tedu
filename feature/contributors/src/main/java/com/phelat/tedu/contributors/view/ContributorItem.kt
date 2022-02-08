2e30a4884dec8cfa2b3a747cf20eb4ea23108506
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2022-02-08 22:21:04 +0100

    Make the project buildable once again

--- feature/contributors/src/main/java/com/phelat/tedu/contributors/view/ContributorItem.kt
+++ feature/contributors/src/main/java/com/phelat/tedu/contributors/view/ContributorItem.kt
@@ -1,23 +1,25 @@
 package com.phelat.tedu.contributors.view
 
+import android.view.View
 import com.phelat.tedu.contributors.R
+import com.phelat.tedu.contributors.databinding.ItemContributorBinding
 import com.phelat.tedu.contributors.entity.ContributorEntity
-import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
-import com.xwray.groupie.kotlinandroidextensions.Item
-import kotlinx.android.synthetic.main.item_contributor.contributionNumber
-import kotlinx.android.synthetic.main.item_contributor.contributionTitle
-import kotlinx.android.synthetic.main.item_contributor.contributor
+import com.xwray.groupie.viewbinding.BindableItem
 
 class ContributorItem(
     private val entity: ContributorEntity,
     private val onClickListener: (ContributorEntity) -> Unit
-) : Item(entity.hashCode().toLong()) {
+) : BindableItem<ItemContributorBinding>(entity.hashCode().toLong()) {
 
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.contributionNumber.text = entity.contributionNumber
-        viewHolder.contributor.text = entity.contributor
-        viewHolder.contributionTitle.text = entity.contribution
-        viewHolder.itemView.setOnClickListener { onClickListener.invoke(entity) }
+    override fun bind(viewBinding: ItemContributorBinding, position: Int) {
+        viewBinding.contributionNumber.text = entity.contributionNumber
+        viewBinding.contributor.text = entity.contributor
+        viewBinding.contributionTitle.text = entity.contribution
+        viewBinding.root.setOnClickListener { onClickListener.invoke(entity) }
+    }
+
+    override fun initializeViewBinding(view: View): ItemContributorBinding {
+        return ItemContributorBinding.bind(view)
     }
 
     override fun getLayout(): Int = R.layout.item_contributor
