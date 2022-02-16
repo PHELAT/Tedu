60f0753c332eff422a739683cf5e890c7159cb98
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2022-02-16 22:46:59 +0100

    Migrate :feature:addtodo module to viewbinding

--- feature/addtodo/src/main/java/com/phelat/tedu/addtodo/view/calendar/HeaderViewHolder.kt
+++ feature/addtodo/src/main/java/com/phelat/tedu/addtodo/view/calendar/HeaderViewHolder.kt
@@ -1,18 +1,19 @@
 package com.phelat.tedu.addtodo.view.calendar
 
-import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.isVisible
 import com.kizitonwose.calendarview.model.CalendarMonth
 import com.kizitonwose.calendarview.ui.ViewContainer
+import com.phelat.tedu.addtodo.databinding.ItemCalendarHeaderBinding
 import com.phelat.tedu.addtodo.viewmodel.DateViewModel
-import kotlinx.android.synthetic.main.item_calendar_header.view.headerMonth
-import kotlinx.android.synthetic.main.item_calendar_header.view.headerYear
 
-class HeaderViewHolder(view: View, private val dateViewModel: DateViewModel) : ViewContainer(view) {
+class HeaderViewHolder(
+    binding: ItemCalendarHeaderBinding,
+    private val dateViewModel: DateViewModel
+) : ViewContainer(binding.root) {
 
-    private val headerYear: AppCompatTextView = view.headerYear
-    private val headerMonth: AppCompatTextView = view.headerMonth
+    private val headerYear: AppCompatTextView = binding.headerYear
+    private val headerMonth: AppCompatTextView = binding.headerMonth
 
     fun bind(month: CalendarMonth) {
         dateViewModel.onBindCalendarHeader(month).apply {
